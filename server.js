66c9bb498f82e9becfa2cf79711d63a17000753b
Lev <leevkrasnov@gmail.com>
2024-12-16 20:34:00 +0500

    feat: make express server

---
+++ server.js
@@ -0,0 +1,31 @@
+import express from 'express';
+import cors from 'cors';
+import axios from 'axios';
+
+const app = express();
+const PORT = process.env.PORT || 5001;
+
+app.use(cors());
+
+app.get('/api/proxy', async (req, res) => {
+  const { url } = req.query;
+
+  if (!url) {
+    return res.status(400).json({ error: 'URL параметр обязателен' });
+  }
+
+  try {
+    const response = await axios.get(url, {
+      responseType: url.includes('.xml') ? 'text' : 'json',
+    });
+
+    res.send(response.data);
+  } catch (error) {
+    console.error('Ошибка при запросе:', error.message);
+    res.status(500).json({ error: 'Ошибка при запросе к API' });
+  }
+});
+
+app.listen(PORT, () => {
+  console.log(`Сервер запущен на http://localhost:${PORT}`);
+});
